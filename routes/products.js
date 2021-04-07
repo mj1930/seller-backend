2c0dece96ccad99cd82859621e06d063d9f2e489
Mihir Jain <mj3003@Mihirs-MacBook-Air.local>
2021-04-07 22:57:01 +0530

    change

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const { upload } = require('../helpers/file-upload');
 router.post('/add-product-info', authorize, productCtrl.addNewProduct);
 router.post('/add-product-variation', authorize, productCtrl.addProductVariation);
 router.post('/add-product-selling-info', authorize, productCtrl.addProductSellingInfo);
-router.post('/add-product-images', authorize, upload.array('producImage', 2), productCtrl.addProductImages);
+router.post('/add-product-images', authorize, upload.array('productImage', 2), productCtrl.addProductImages);
 router.post('/add-product-desc-info', authorize, productCtrl.addProductDesc);
 router.post('/get-all-products', productCtrl.listAllProduct);
 router.post('/filter-products', productCtrl.filterProducts);
